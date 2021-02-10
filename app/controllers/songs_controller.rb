d743f5f18782af200673186148891e506204dcf0
puifaiba <puifai.ba@gmail.com>
2021-02-09 21:47:10 -0500

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,57 @@
+class SongsController < ApplicationController
+
+    before_action :get_song, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @songs = Song.all
+    end
+
+    def show
+    end
+
+    def new
+        @song = Song.new
+    end
+
+    def create
+        @song = Song.new(song_params)
+        if @song.valid?
+            @song.save
+            redirect_to song_path(@song)
+        else
+            flash[:error] = @song.errors.full_messages
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @song.update(song_params)
+        if @song.valid?
+            @song.save
+            redirect_to song_path(@song)
+        else
+            flash[:error] = @song.errors.full_messages
+            render :edit
+        end
+    end
+
+    def destroy 
+        @song.destroy
+        redirect_to songs_path
+    end
+
+    private
+
+    def get_song
+        @song = Song.find_by(id: params[:id])
+    end
+
+    def song_params
+        params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+    end
+
+
+end

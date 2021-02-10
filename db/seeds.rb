e56bfbbe21043cc33aff107f4bbd2e6fe1e23705
puifaiba <puifai.ba@gmail.com>
2021-02-09 21:47:05 -0500

    commit tree 4052e12d489b6ef6a33a1daf8ccdf0b7076deb33

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+
+
+song1 = Song.create!(title: "Happy", released: true, release_year: 2015, artist_name: "Pharrell", genre: "pop")
+song2 = Song.create!(title: "What Now?", released: false, release_year: nil, artist_name: "Rihanna", genre: "rnb")
+song3 = Song.create!(title: "Highway to Hell", released: true, release_year: 1979, artist_name: "AC/DC", genre: "rock")
